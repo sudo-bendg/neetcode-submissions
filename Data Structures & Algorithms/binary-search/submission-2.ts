69d5f24a00c10a4d69b5a32e0e1abe381705c1e3
Benjamin Godfrey <benjamin.godfrey.dev@gmail.com>
2026-09-18 23:52:37 +0100

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let left = 0;
+        let right = nums.length - 1;
+        let middle = Math.floor((left + right) / 2);
+
+        while (left <= right) {
+            if (nums[middle] === target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1
+            }
+            middle = Math.floor((left + right) / 2);
+        }
+
+        return -1;
+    }
+}
